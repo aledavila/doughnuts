0384f9224d2cdce780725b57efc863145e6496c6
Kelli Waiss <kelliwaiss@hotmail.com>
2016-02-05 15:38:01 -0600

    working on delete deature

--- starter-code/app/js/app.js
+++ starter-code/app/js/app.js
@@ -5,7 +5,8 @@ $(function() {
     var appendDoughnut = function(doughnut) {
 
         $('#doughnuts').prepend('<a href="#"> ' + '<li class="doughnut" data-doughnut-flavor="' + doughnut.flavor +
-            '" data-doughnut-style="' + doughnut.style + '">' +
+            '" data-doughnut-style="' + doughnut.style + 
+            '" data-doughnut-id="' + doughnut.id + '">' +
             '<span class="italic">' + doughnut.flavor +'</span>' +
             '<span class="glyphicon glyphicon-heart-empty" id="heart"></span>' + doughnut.style +
             '</li></a>');
@@ -20,13 +21,6 @@ $(function() {
            var doughnut = doughnuts[i];
            appendDoughnut(doughnut);
        }
-        
-        $('.doughnut').click(function() {
-        var doughnutId = $(this).data('doughnut-id');
-        console.log('Doughnut Id to delete: ', doughnutId);
-
-        // deleteDoughnut(doughnutId);
-    });
 
    };
 
@@ -38,11 +32,21 @@ $(function() {
        .done(function(data) {
            var doughnuts = data;
            displayDoughnuts(doughnuts);
+
            $('.doughnut').on('click', function() {
-            var flavor = $(this).data('doughnut-flavor');
-            var style = $(this).data('doughnut-style');
-            $('#modal .header').text(flavor + ' - ' + style);
-             $('#modal').modal('show');
+                var flavor = $(this).data('doughnut-flavor');
+                var style = $(this).data('doughnut-style');
+                var doughnutId = $(this).data('doughnut-id');
+
+                $('#modal .header').text(flavor + ' - ' + style);
+                $('#modal').modal('show');
+
+                $('#delete').click(function() {
+                    // var doughnutId = $(this).data('doughnut-id');
+                    console.log('Doughnut Id to delete: ', doughnutId);
+
+                    deleteDoughnut(doughnutId);
+                });
            });
        })
        .fail(function(err) {
@@ -77,26 +81,27 @@ $(function() {
 
    });
 
-    // var deleteDoughnut = function(doughnutId){
+    var deleteDoughnut = function(doughnutId){
   
-    //    $.ajax({
-    //        url: 'https://api.doughnuts.ga/doughnuts' + '',
-    //        method: "DELETE",
-    //        data: {},
-    //        dataType: "json"
-    //    })
-    //    .done(function(data) {
-    //     console.log('deleted!');
-
-    //    })
-    //    .fail(function(err) {
-    //        if (err) throw err;
-    //    })
-    //    .always(function() {
-    //        console.log('Request completed');
-    //     });
-    //    console.log(flavor, style);
+       $.ajax({
+           url: 'https://api.doughnuts.ga/doughnuts/' + doughnutId,
+           method: "DELETE",
+           data: {},
+           dataType: "json"
+       })
+       .done(function(data) {
+            console.log('deleted!');
+            $(this).remove(".doughnut");
+            $('#modal').modal('hide');
+       })
+       .fail(function(err) {
+           if (err) throw err;
+       })
+       .always(function() {
+           // console.log('Request completed');
+        });
+       // console.log(flavor, style);
     
-    // };
+    };
     
 });
