48f7c15058d133604d0cf220e2c80fe003570082
Kelli Waiss <kelliwaiss@hotmail.com>
2016-02-05 12:23:24 -0600

    added link

--- starter-code/app/js/app.js
+++ starter-code/app/js/app.js
@@ -1,64 +1,64 @@
 // jQuery Document Ready
 $(function() {
-    // var apiRoot = 'https://api.doughnuts.ga/doughnuts';
+   // var apiRoot = 'https://api.doughnuts.ga/doughnuts';
 
-    $.ajax({
-            url: 'https://api.doughnuts.ga/doughnuts',
-            method: "GET",
-            data: {}
-        })
-        .done(function(data) {
-            var doughnuts = data;
-            displayDoughnuts(doughnuts);
-        })
-        .fail(function(err) {
-            if (err) throw err;
-        })
+   $.ajax({
+           url: 'https://api.doughnuts.ga/doughnuts',
+           method: "GET",
+           data: {}
+       })
+       .done(function(data) {
+           var doughnuts = data;
+           displayDoughnuts(doughnuts);
+       })
+       .fail(function(err) {
+           if (err) throw err;
+       })
 
     var appendDoughnut = function(doughnut) {
-        $('#doughnuts').append('<li class="doughnut">' +
+        $('#doughnuts').append('<a href=""> ' + '<li class="doughnut">' +
             doughnut.style + ' - ' +
-            doughnut.flavor + '</li>');
+            doughnut.flavor + '</li>' + '</a>');
     }
 
-    var displayDoughnuts = function(doughnuts) {
-        if (!doughnuts) {
-            return;
-        }
+   var displayDoughnuts = function(doughnuts) {
+       if (!doughnuts) {
+           return;
+       }
 
-        for (var i = 0; i < doughnuts.length; i++) {
-            var doughnut = doughnuts[i];
-            appendDoughnut(doughnut);
-        }
-    }
+       for (var i = 0; i < doughnuts.length; i++) {
+           var doughnut = doughnuts[i];
+           appendDoughnut(doughnut);
+       }
+   }
 
-    $('#new-doughnut').submit(function(event){
-        event.preventDefault();
+   $('#new-doughnut').submit(function(event){
+       event.preventDefault();
 
-        var flavor = $('#doughnut-flavor').val();
-        var style = $('#doughnut-style option:selected').val();
+       var flavor = $('#doughnut-flavor').val();
+       var style = $('#doughnut-style option:selected').val();
 
-        $.ajax({
-            url: 'https://api.doughnuts.ga/doughnuts',
-            method: "POST",
-            data: {
-                flavor: flavor,
-                style: style
-            },
-            dataType: "json"
-        })
-        .done(function(data) {
-            appendDoughnut(data);
-        })
-        .fail(function(err) {
-            if (err) throw err;
-        })
-        .always(function() {
-            console.log('Request completed');
-          });
-        console.log(flavor, style);
+       $.ajax({
+           url: 'https://api.doughnuts.ga/doughnuts',
+           method: "POST",
+           data: {
+               flavor: flavor,
+               style: style
+           },
+           dataType: "json"
+       })
+       .done(function(data) {
+           appendDoughnut(data);
+       })
+       .fail(function(err) {
+           if (err) throw err;
+       })
+       .always(function() {
+           console.log('Request completed');
+         });
+       console.log(flavor, style);
 
-    });
+   });
 
 
 
